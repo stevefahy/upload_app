7fb562cce4875d7ba4f4acb8e4c7211561338c64
stevefahy <stevefahydev@gmail.com>
2017-08-28 10:30:42 +0100

    added CORS for localhost

--- app.js
+++ app.js
@@ -11,11 +11,19 @@ var port = process.env.PORT || 8060;
 // Configuration
 app.use(express.static(__dirname + '/public'));
 app.use(connect.cookieParser());
-app.use(connect.logger('dev')); 
+app.use(connect.logger('dev'));
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
- 
+
+// CORS for localhost testing
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 // Routes
 require('./routes/routes.js')(app);
 
